da9a05a969f061b4d3de25d900cd6127bb69a6f8
Blessing Tutka <benedicte.tutekamukuta@student.unamur.be>
2026-02-11 15:13:26 +0100

    chore: update eslint

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -15,6 +16,9 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      'unused-imports': unusedImports,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
