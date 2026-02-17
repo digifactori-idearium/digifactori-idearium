9ba2060831ee28c8444ac378f92a924cfd444d33
Robin23D <robin.delvaux@student.unamur.be>
2026-02-17 11:58:33 +0100

    feat(web/profile): première version profile

--- apps/web/src/routes/public.routes.tsx
+++ apps/web/src/routes/public.routes.tsx
@@ -1,9 +1,11 @@
 import AuthPage from '@/pages/Auth';
 import Home from '@/pages/Home';
+import ProfilePage from '@/pages/Profile';
 
 const publicRoutes = [
   { path: '', element: <Home />, exact: true },
   { path: 'auth', element: <AuthPage /> },
+  {path: 'profile', element: <ProfilePage/>}
 ];
 
 export default publicRoutes;
