d819d0ad140cf3121e246af7f66b60a78c7e98fb
Robin23D <robin.delvaux@student.unamur.be>
2026-02-17 12:20:47 +0100

    perf(web/profile): changement d'URL

--- apps/web/src/services/profile.service.ts
+++ apps/web/src/services/profile.service.ts
@@ -18,7 +18,7 @@ interface ApiResponse<T> {
 // Services
 export const getProfile = async (empId: string): Promise<ApiResponse<Profile>> => {
     try {
-        const response: AxiosResponse<ApiResponse<Profile>> = await axios.get(`/profile/code=125`);
+        const response: AxiosResponse<ApiResponse<Profile>> = await axios.get(`http://localhost:3001/api/profile/code=125`);
         return response.data;
     } catch (error: any) {
         throw new Error(error.response?.data?.error?.message || 'Error fetching employer profile');
