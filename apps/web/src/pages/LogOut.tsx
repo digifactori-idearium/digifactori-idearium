f1a5057e1e1bea594fd9b08ad45415ca6bd80ea6
Robin23D <robin.delvaux@student.unamur.be>
2026-02-19 16:50:52 +0100

    feat(web/auth): log out

---
+++ apps/web/src/pages/LogOut.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Home from "./Home";
+
+import { useUser } from '@/providers/UserProvider';
+
+const LogOut: React.FC = () => {
+
+    const user = useUser()
+    user.removeToken();
+
+  return (
+    <Home />
+  );
+};
+
+export default LogOut;
