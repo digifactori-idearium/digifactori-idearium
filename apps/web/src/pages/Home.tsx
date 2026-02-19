6d28f1d8b3fb77bcc5c8da00681246e88a36e0da
qdewee <115534044+qdewee@users.noreply.github.com>
2026-02-19 16:15:37 +0100

    style(web/home) : création et ajout des boutons hexagonaux pour former le nid d'abeille sur la page home.

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from '@/components/ui/button';
 
 const Home: React.FC = () => {
   return (
@@ -12,6 +13,35 @@ const Home: React.FC = () => {
       <button className="mt-6 px-6 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition">
         Get Started
       </button>
+
+
+      <div className="flex flex-col items-center mt-5">
+        <div className="flex gap-0 -mt-0">
+          <Button variant="hex" size="hex" onClick={() => alert("Kitchen clicked")}>
+            Room
+          </Button>
+          <Button variant="hex" size="hex" onClick={() => alert("Kitchen clicked")}>
+            Room2
+          </Button>
+          <Button variant="hex" size="hex" onClick={() => alert("Kitchen clicked")}>
+            Room3
+          </Button>
+        </div>
+        <div className="flex gap-0 -mt-0 absolute mt-19">
+          <Button variant="hex" size="hex" onClick={() => alert("Kitchen clicked")}>
+            Room4
+          </Button>
+          <Button variant="hex" size="hex" onClick={() => alert("Kitchen clicked")}>
+            Room5
+          </Button>
+          <Button variant="hex" size="hex" onClick={() => alert("Kitchen clicked")}>
+            Room6
+          </Button>
+          <Button variant="hex" size="hex" onClick={() => alert("Kitchen clicked")}>
+            Room7
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
