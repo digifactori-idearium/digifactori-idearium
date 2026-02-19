de8dd2a112981d40c200dfe98fe9ec6fc097066e
Blessing Tutka <blessigtutka>
2026-02-19 14:43:08 +0100

    feat(components): add dynamic form

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter(routes);
 
 const App = () => {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <UserProvider>
         <TooltipProvider>
           <RouterProvider router={router} />
