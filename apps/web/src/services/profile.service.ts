84e1e23a92b41f76484190ab21d409d39ac78883
Robin23D <robin.delvaux@student.unamur.be>
2026-02-19 14:41:12 +0100

    feat(web/profile): access + update profile (frontend)

--- apps/web/src/services/profile.service.ts
+++ apps/web/src/services/profile.service.ts
@@ -17,12 +17,25 @@ interface ApiResponse<T> {
 }
 
 // Services
-export const getProfile = async (): Promise<ApiResponse<getProfileResponse>> => {
-    console.log("getProfile...")
+export const getProfile = async (parentalCode: string = ""): Promise<ApiResponse<getProfileResponse>> => {
     try {
-        const response: AxiosResponse<ApiResponse<getProfileResponse>> = await axios.post(`http://localhost:3001/api/profile/`, {});
+        const response: AxiosResponse<ApiResponse<getProfileResponse>> = await axios.post(`http://localhost:3001/api/profile/`, 
+            parentalCode == "" ? {} : {parental_code: parentalCode});
         return response.data;
     } catch (error: any) {
         throw new Error(error.response?.data?.error?.message || 'Error fetching employer profile');
     }
 };
+
+export const updateProfile = async (password: string, newUserInfo: Partial<User>, newProfileInfo: Partial<ProfileType>) => {
+    try {
+        const response: AxiosResponse<ApiResponse<getProfileResponse>> = await axios.post(`http://localhost:3001/api/profile/setting`, {
+            password: password,
+            user: newUserInfo,
+            profile: newProfileInfo
+        });
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.error?.message || 'Error fetching employer profile' + error);
+    }
+}
