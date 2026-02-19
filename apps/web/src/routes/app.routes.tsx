5fa6e68ae8f8a93e0a9448b3a9b2fac6ce2f3837
Robin23D <robin.delvaux@student.unamur.be>
2026-02-19 14:52:27 +0100

    refactor(web/profile): linter corrections

--- apps/web/src/routes/app.routes.tsx
+++ apps/web/src/routes/app.routes.tsx
@@ -1,9 +1,7 @@
 import Dashboard from '@/pages/Dashboard';
-import UpdateProfilePage from '../pages/UpdateProfile';
 
 const appRoutes = [
     {path: '', element: <Dashboard />, exact: true },
-    {path: 'profile/update', element: <UpdateProfilePage />, exact: true },
 ];
 
 export default appRoutes;
