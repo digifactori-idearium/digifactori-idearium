6b3e2e794305a1f3362ecf4bbc7ae197e110c16b
Mersa Kuljici <mersa.kuljici@student.unamur.be>
2026-02-19 19:53:39 +0100

    style(web/app): add home page information

--- apps/web/src/components/home/Card.tsx
+++ apps/web/src/components/home/Card.tsx
@@ -1,6 +1,6 @@
 import Aos from 'aos';
-import React, { useEffect } from 'react';
 import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 function Card(props: { animation: string; image: string }) {
   useEffect(function () {
