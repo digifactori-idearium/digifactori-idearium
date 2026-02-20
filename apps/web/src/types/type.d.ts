bb3947fab1f513d65222c4bd7fc4971bc27cd9f0
Blessing Tutka <blessigtutka>
2026-02-20 11:09:46 +0100

    refactor(web/types): update type

--- apps/web/src/types/type.d.ts
+++ apps/web/src/types/type.d.ts
@@ -12,17 +12,24 @@ type User = {
   last_name: string;
   password: string;
   parental_code: string | null;
-  role: "CHILD" | "SUPERVISOR";
-}
+  role: 'CHILD' | 'SUPERVISOR';
+};
+
+type UserSession = {
+  id: string;
+  email: string;
+  role: 'CHILD' | 'SUPERVISOR';
+  token: string;
+};
 
 type Profile = {
-    id: string;
-    userId: string;
-    pseudo: string;
-    avatar: string | null;
-    bio: string | null;
-    createdAt: Date;
-    updatedAt: Date;
-}
+  id: string;
+  userId: string;
+  pseudo: string;
+  avatar: string | null;
+  bio: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+};
 
-type Role = "CHILD" | "SUPERVISOR"
+type Role = 'CHILD' | 'SUPERVISOR';
