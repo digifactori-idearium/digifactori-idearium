5dce876ddafb4629ada9827150c3978c9b204a04
Blessing Tutka <benedicte.tutekamukuta@student.unamur.be>
2026-02-20 11:02:59 +0100

    refactor(web/pages): update profile

--- apps/web/src/pages/Profile.tsx
+++ apps/web/src/pages/Profile.tsx
@@ -1,203 +1,318 @@
+import { Loader2, Lock, Trash2, UserCircle, X } from 'lucide-react';
 import React, { useEffect, useState } from 'react';
+import { toast } from 'sonner';
 
 import { getProfile, updateProfile } from '../services/profile.service';
 
-const Profile: React.FC = () => {
-  const getProfileWithParentalCode = async (parentalCode: string) => {
-    const response = await getProfile(parentalCode);
-    console.log('with parentalCode: ', response.data);
-    return response.data;
-  };
+import Form from '@/components/global/Form';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { useUser } from '@/providers/UserProvider';
 
-  const [user, setUser] = useState<{ profile: Profile; user?: User } | null>(
-    null
+const AVATAR_OPTIONS = [
+  { id: 1, url: 'https://api.dicebear.com/7.x/adventurer/svg?seed=Noah' },
+  { id: 2, url: 'https://api.dicebear.com/7.x/adventurer/svg?seed=Felix' },
+  { id: 3, url: 'https://api.dicebear.com/7.x/bottts/svg?seed=Emma' },
+  { id: 4, url: 'https://api.dicebear.com/7.x/adventurer/svg?seed=Sophie' },
+  { id: 5, url: 'https://api.dicebear.com/7.x/adventurer/svg?seed=Lily' },
+];
+
+const ProfilePage: React.FC = () => {
+  const { getUser } = useUser();
+  const [acc, setAcc] = useState<{ profile: any; user?: any } | null>(null);
+  const [parentalAccess, setParentalAccess] = useState(
+    getUser()?.role != 'CHILD'
   );
-  const [parentalCode, setParentalCode] = useState('1234');
+  const [parentalCodeInput, setParentalCodeInput] = useState('');
+  const [open, setOpen] = useState(false);
 
-  const [password, setPassword] = useState('');
-
-  // localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbWx0aTFoaG8wMDAwN2t1dGZnZGVxaWN3IiwiZW1haWwiOiJyb2JpbjNAZ21haWwuY29tIiwicm9sZSI6IkNISUxEIiwiaWF0IjoxNzcxNTA3ODYwLCJleHAiOjE3NzE1MjIyNjB9.541Cv5cdZyxwAApQJXVqODGhartaPkVuTerq6w5ybig");
-
+  // Fetch profile on mount
   useEffect(() => {
     const fetchProfile = async () => {
       try {
-        const response = await getProfile();
-        setUser(response.data);
-      } catch (err: any) {
-        console.log('error: ', err);
+        const res = await getProfile();
+        setAcc(res.data);
+      } catch (err) {
+        console.error(err);
       }
     };
     fetchProfile();
   }, []);
 
-  const [updatedUser, setUpdatedUser] = useState(
-    user?.user ?? {
-      email: '',
-      first_name: '',
-      last_name: '',
-      password: '',
-      parental_code: '',
-      role: 'CHILD' as Role,
-    }
-  );
-
-  const [updatedProfile, setUpdatedProfile] = useState({
-    pseudo: user?.profile.pseudo,
-    bio: user?.profile.bio ?? '',
-    avatar: user?.profile.avatar ?? '',
-  });
-
-  useEffect(() => {
-    if (user?.user) {
-      // eslint-disable-next-line react-hooks/set-state-in-effect
-      setUpdatedUser({
-        email: user.user.email,
-        first_name: user.user.first_name,
-        last_name: user.user.last_name,
-        password: user.user.password,
-        parental_code: user.user.parental_code ? user.user.parental_code : '',
-        role: user.user.role,
-      });
-      if (user?.profile) {
-        setUpdatedProfile({
-          pseudo: user?.profile.pseudo,
-          bio: user.profile.bio ?? '',
-          avatar: user.profile.avatar ?? '',
-        });
+  const handleUnlock = async () => {
+    console.log(acc?.user);
+    if (parentalCodeInput) {
+      setParentalAccess(true);
+      const response = await getProfile(parentalCodeInput);
+      if (response.data.user) {
+        setAcc(response.data);
+        toast.success('You can configure your account');
       }
+    } else {
+      toast.error('Incorrect parental code');
     }
-  }, [user]);
+  };
 
-  if (user?.user) {
+  if (!acc) {
     return (
-      <div className="w-full min-h-screen flex flex-col items-center justify-center px-6">
-        <h1 className="text-4xl font-bold text-gray-800 mb-4">
-          Ton profiiiiile{} 👋
-        </h1>
-        <span>
-          {' '}
-          mdp (pour modifier){' '}
-          <input
-            type="text"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          ></input>
-        </span>
-        {/* <span> e-mail: <input type="text" value={user.user.email} onChange={e => setUser({...user, user: {...user.user as User, email: e.target.value}})}></input></span> */}
-        <span>
-          {' '}
-          prénom:{' '}
-          <input
-            type="text"
-            value={updatedUser.email}
-            onChange={e =>
-              setUpdatedUser({ ...updatedUser, email: e.target.value })
-            }
-          ></input>
-        </span>
-        <span>
-          {' '}
-          prénom:{' '}
-          <input
-            type="text"
-            value={updatedUser.first_name}
-            onChange={e =>
-              setUpdatedUser({ ...updatedUser, first_name: e.target.value })
-            }
-          ></input>
-        </span>
-        <span>
-          {' '}
-          nom de famille:{' '}
-          <input
-            type="text"
-            value={updatedUser.last_name}
-            onChange={e =>
-              setUpdatedUser({ ...updatedUser, last_name: e.target.value })
-            }
-          ></input>
-        </span>
-        <span>
-          mot de passe:{' '}
-          <input
-            type="text"
-            value={updatedUser.password}
-            onChange={e =>
-              setUpdatedUser({ ...updatedUser, password: e.target.value })
-            }
-          ></input>{' '}
-        </span>
-        {updatedUser.parental_code && updatedUser.parental_code != '' && (
-          <span>
-            code parental
-            <input
-              type="text"
-              value={updatedUser.parental_code}
-              onChange={e =>
-                setUpdatedUser({
-                  ...updatedUser,
-                  parental_code: e.target.value,
-                })
-              }
-            ></input>{' '}
-          </span>
-        )}
-        <span>
-          pseudo:{' '}
-          <input
-            type="text"
-            value={updatedProfile.pseudo}
-            onChange={e =>
-              setUpdatedProfile({ ...updatedProfile, pseudo: e.target.value })
-            }
-          ></input>
-        </span>
-        <span>
-          bio:{' '}
-          <input
-            type="text"
-            value={updatedProfile.bio}
-            onChange={e =>
-              setUpdatedProfile({ ...updatedProfile, bio: e.target.value })
-            }
-          ></input>
-        </span>
-        <span>
-          avatar:{' '}
-          <input
-            type="text"
-            value={updatedProfile.avatar}
-            onChange={e =>
-              setUpdatedProfile({ ...updatedProfile, avatar: e.target.value })
-            }
-          ></input>
-        </span>
-        <button
-          onClick={() => updateProfile(password, updatedUser, updatedProfile)}
-        ></button>
+      <div className="w-full min-h-screen flex items-center justify-center">
+        <Loader2 /> Loading
       </div>
     );
   }
 
+  // Form initial values
+  const initialValues = {
+    pseudo: acc.profile.pseudo || '',
+    bio: acc.profile.bio || '',
+    avatar: acc.profile.avatar || AVATAR_OPTIONS[0].url,
+  };
+
+  const advancedInitialValues = {
+    email: acc.user?.email || '',
+    first_name: acc.user?.first_name || '',
+    last_name: acc.user?.last_name || '',
+    role: acc.user?.role || '',
+    parental_code: '',
+  };
+
+  const handleSubmit = async (data: any) => {
+    try {
+      await updateProfile(acc.user, data);
+      setAcc(prev => (prev ? { ...prev, profile: data } : prev));
+      toast.success('Profil mis à jour !');
+    } catch (err) {
+      console.error(err);
+      toast.error('Échec de la mise à jour du profil.');
+    }
+  };
+
+  const handleAdvancedSubmit = async (data: any) => {
+    try {
+      await updateProfile(data, acc.profile);
+      setAcc(prev =>
+        prev ? { ...prev, user: { ...prev.user, ...data } } : prev
+      );
+      toast.success('Profil mis à jour !');
+    } catch {
+      toast.error('Échec de la mise à jour du profil.');
+    }
+  };
+
   return (
-    <div className="w-full min-h-screen flex flex-col items-center justify-center px-6">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">Ton profile 👋</h1>
-      <span>pseudo: {user ? user.profile.pseudo : 'pas connecté'}</span>
-      <span>bio: {user ? user.profile.bio : 'pas connecté'}</span>
-      <span>avatar: {user ? user.profile.avatar : 'pas connecté'}</span>
-      <p>Votre code parental</p>
-      <input
-        type="text"
-        value={parentalCode}
-        onChange={e => setParentalCode(e.target.value)}
-      ></input>
-      <button
-        onClick={() =>
-          getProfileWithParentalCode(parentalCode).then(res => setUser(res))
-        }
-      ></button>
+    <div className="w-full">
+      <div className="flex gap-2 justify-center items-center py-4">
+        <UserCircle className="w-12! h-12! text-mauve" />
+        <h1 className="magic-text text-center w-fit md:text-5xl! text-3xl!">
+          MON PROFILE
+        </h1>
+      </div>
+
+      <div className="h-full w-full flex md:flex-row flex-col justify-center items-center md:gap-8 gap-1">
+        {/* Avatar */}
+        <div className="flex flex-col gap-3 justify-center items-center md:w-2/6 w-full">
+          <div className="w-32 h-32 rounded-full border-4 p-1">
+            <Avatar className="w-full h-full">
+              <AvatarImage src={initialValues.avatar} />
+              <AvatarFallback>NB</AvatarFallback>
+            </Avatar>
+          </div>
+          <p className="magic-text py-1!">{acc.profile.pseudo}</p>
+          {/* Avatar Selection */}
+          <div className="grid grid-cols-3 grid-rows-2 gap-6 mb-10 w-fit mx-auto">
+            {AVATAR_OPTIONS.map(av => (
+              <button
+                key={av.id}
+                onClick={() =>
+                  setAcc(prev =>
+                    prev
+                      ? {
+                          ...prev,
+                          profile: { ...prev.profile, avatar: av.url },
+                        }
+                      : prev
+                  )
+                }
+                className={`w-16 h-16 p-1! rounded-full overflow-hidden border-2 transition-all ${
+                  acc.profile.avatar === av.url
+                    ? 'border-orange-500 scale-110 shadow-md'
+                    : 'border-transparent opacity-50'
+                }`}
+              >
+                <img
+                  src={av.url}
+                  alt="avatar option"
+                  className="w-full h-full object-cover"
+                />
+              </button>
+            ))}
+          </div>
+        </div>
+
+        {/* Profile Form */}
+        <div className="lg:4/6 md:w-3/6 w-full p-3">
+          <Form
+            inputs={[
+              { label: 'Pseudo', type: 'text', name: 'pseudo', required: true },
+              { label: 'Bio', type: 'textarea', name: 'bio' },
+              // { label: 'Avatar URL', type: 'text', name: 'avatar' },
+            ]}
+            initialValues={initialValues}
+            handleOnSubmit={handleSubmit}
+          />
+
+          {/* Advanced Settings */}
+          <Dialog
+            open={open}
+            onOpenChange={isOpen => {
+              setOpen(isOpen);
+
+              if (!isOpen) {
+                setParentalAccess(getUser()?.role != 'CHILD');
+                setParentalCodeInput('');
+              }
+            }}
+          >
+            <DialogTrigger asChild>
+              <Button className="flex items-center gap-2 bg-mauve! text-white mt-6">
+                <Lock className="w-4 h-4" />
+                Advanced Settings
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-md max-h-[90vh] flex flex-col overflow-hidden bg-sidebar! border-mauve! [&>button]:hidden">
+              <DialogHeader>
+                <DialogTitle>
+                  {parentalAccess ? 'Account Setting' : 'Enter Parental Code'}
+                </DialogTitle>
+              </DialogHeader>
+
+              {parentalAccess && (
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <div className="absolute top-3 right-14">
+                      <Button className=" w-8 h-8 flex justify-center items-center bg-red-300! text-red-700! rounded-full">
+                        <Trash2 className="w-6 h-6" />
+                      </Button>
+                    </div>
+                  </AlertDialogTrigger>
+
+                  <AlertDialogContent className="rounded-4xl border-none shadow-2xl p-8">
+                    <AlertDialogHeader className="flex flex-col items-center justify-center">
+                      <AlertDialogTitle className="text-2xl mx-auto font-black text-slate-800 text-center!">
+                        Are you sure?
+                      </AlertDialogTitle>
+                      <AlertDialogDescription className="text-slate-500 text-center text-lg">
+                        This will permanently delete the profile for{' '}
+                        <span className="font-bold text-slate-800">
+                          {acc.profile.pseudo}
+                        </span>
+                        . This action cannot be undone.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+
+                    <AlertDialogFooter className="flex-col sm:flex-row gap-3 mt-4">
+                      <AlertDialogCancel className="p-4 rounded-2xl border-2 border-slate-100 font-bold text-slate-500">
+                        No, keep it
+                      </AlertDialogCancel>
+                      <AlertDialogAction
+                        onClick={() => console.log('Deleting profile...')}
+                        className="p-4 rounded-2xl bg-red-500! hover:bg-red-600! font-bold shadow-[0_4px_0_rgb(180,40,40)] active:translate-y-1 active:shadow-none transition-all"
+                      >
+                        Yes, delete profile
+                      </AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              )}
+
+              <DialogClose
+                className="absolute w-8 h-8 flex justify-center items-center top-3 right-3 text-white hover:text-white/80 bg-mauve! rounded-full"
+                asChild
+              >
+                <X className="w-6 h-6" />
+              </DialogClose>
+
+              <div className="flex-1 overflow-y-auto pr-2 custom-scrollbar">
+                {parentalAccess ? (
+                  <div className="p-3">
+                    <Form
+                      inputs={[
+                        {
+                          label: 'Email',
+                          type: 'email',
+                          name: 'email',
+                          required: true,
+                        },
+                        {
+                          label: 'First Name',
+                          type: 'text',
+                          name: 'first_name',
+                        },
+                        { label: 'Last Name', type: 'text', name: 'last_name' },
+                        {
+                          label: 'Role',
+                          type: 'select',
+                          name: 'role',
+                          options: [
+                            { text: 'Enfant', value: 'CHILD' },
+                            { text: 'Superviseur', value: 'SUPERVISOR' },
+                          ],
+                        },
+                        {
+                          label: 'Change Parental Code',
+                          type: 'text',
+                          name: 'parental_code',
+                        },
+                      ]}
+                      initialValues={advancedInitialValues}
+                      handleOnSubmit={handleAdvancedSubmit}
+                    />
+                  </div>
+                ) : (
+                  <>
+                    <Input
+                      type="password"
+                      placeholder="0 0 0 0"
+                      maxLength={4}
+                      className="mt-4 mb-4 text-center text-lg"
+                      value={parentalCodeInput}
+                      onChange={e => setParentalCodeInput(e.target.value)}
+                    />
+                    <Button
+                      onClick={handleUnlock}
+                      className="w-full text-foreground!"
+                    >
+                      Unlock
+                    </Button>
+                  </>
+                )}
+              </div>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Profile;
+export default ProfilePage;
