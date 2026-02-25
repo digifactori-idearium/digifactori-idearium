cebf3539215aa64936f6287f9a8c4038cea39f89
Blessing Tutka <blessigtutka>
2026-02-25 17:21:27 +0100

    perf(web/profile): modularize profile

---
+++ apps/web/src/components/profile/ProfileForm.tsx
@@ -0,0 +1,23 @@
+import Form from '@/components/global/Form';
+
+interface Props {
+  initialValues: any;
+  onSubmit: (data: any) => void;
+}
+
+const ProfileForm: React.FC<Props> = ({ initialValues, onSubmit }) => {
+  return (
+    <div className="lg:4/6 md:w-3/6 w-full p-3">
+      <Form
+        inputs={[
+          { label: 'Pseudo', type: 'text', name: 'pseudo', required: true },
+          { label: 'Bio', type: 'textarea', name: 'bio' },
+        ]}
+        initialValues={initialValues}
+        handleOnSubmit={onSubmit}
+      />
+    </div>
+  );
+};
+
+export default ProfileForm;
