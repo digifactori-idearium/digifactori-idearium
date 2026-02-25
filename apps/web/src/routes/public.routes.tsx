3f9266e2d90f140ce995b95e00228259750c2690
karimdurand <115547165+karimdurand@users.noreply.github.com>
2026-02-25 10:32:01 +0100

    feat(routes): add rooms and playground

--- apps/web/src/routes/public.routes.tsx
+++ apps/web/src/routes/public.routes.tsx
@@ -1,8 +1,18 @@
 import Home from '@/pages/Home';
 import TestFormPage from '@/pages/TestForm';
+import Rooms from '@/pages/Rooms';
+import RoomsPlayground from '@/pages/RoomsPlayground';
 
 const publicRoutes = [
   { path: '', element: <Home />, exact: true },
+  {
+    path: 'rooms',
+    children: [
+      { index: true, element: <Rooms /> },
+      { path: 'playground', element: <RoomsPlayground /> },
+    ],
+  },
+
   { path: 'testform', element: <TestFormPage /> },
 ];
 
