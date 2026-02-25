a101a25fc4eaeba0fd33fdcc56f15b9fa071c030
Robin23D <robin.delvaux@student.unamur.be>
2026-02-25 18:44:20 +0100

    feat(web/room): walls of a room

--- apps/web/src/routes/public.routes.tsx
+++ apps/web/src/routes/public.routes.tsx
@@ -1,9 +1,11 @@
 import Home from '@/pages/Home';
+import { Room } from "@/pages/myPages.tsx";
 import TestFormPage from '@/pages/TestForm';
 
 const publicRoutes = [
   { path: '', element: <Home />, exact: true },
   { path: 'testform', element: <TestFormPage /> },
+  {path: 'room', element: <Room/>}
 ];
 
 export default publicRoutes;
