d9fc6990fb71175666a4f90f854048d6dc974737
fkeunang <farida.keunang@student.unamur.be>
2026-03-04 12:59:22 +0100

    fix(3d): fix voxel error import

--- apps/web/src/components/3d/VoxelColor.tsx
+++ apps/web/src/components/3d/VoxelColor.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, ReactNode } from 'react';
+import { useState, useEffect, useRef, ReactNode, JSX } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 
