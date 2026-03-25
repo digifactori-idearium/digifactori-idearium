28ac0e5325a2f9d7ae7a19b5a866a37d46d596ee
Robin23D <robin.delvaux@student.unamur.be>
2026-03-25 16:02:57 +0100

    fix(web/ideorama): correction de bug pour la sauvegarde auto. Bye bye le strict mode

--- apps/web/src/components/3d/index.tsx
+++ apps/web/src/components/3d/index.tsx
@@ -2,7 +2,6 @@ import { useDroppable } from '@dnd-kit/core';
 import { GizmoHelper, GizmoViewport, OrbitControls } from '@react-three/drei';
 import { Canvas, useThree } from '@react-three/fiber';
 import React, { Suspense, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 import * as THREE from 'three';
 import { useSnapshot } from 'valtio';
 
@@ -12,7 +11,6 @@ import { Model } from './Model';
 import { SceneAudio } from './SceneAudio';
 import { SceneGradient } from './SceneGradient';
 
-import { searchIdeorama } from '@/services/ideorama.service';
 import { actions, sceneState } from '@/stores';
 
 const SceneBackground: React.FC<{ color: string }> = ({ color }) => {
@@ -27,25 +25,9 @@ const SceneBackground: React.FC<{ color: string }> = ({ color }) => {
 };
 
 export const Scene: React.FC = () => {
-  const { ideoramaid } = useParams();
   const snap = useSnapshot(sceneState);
 
-  useEffect(() => {
-    if (!ideoramaid) return;
-
-    searchIdeorama(ideoramaid).then(res => {
-      res.data.model.info.name = res.data.name
-      const model = res.data.model;
-      if (!model) return;
-
-      if (model.global) Object.assign(sceneState.global, model.global);
-      if (model.background)
-        Object.assign(sceneState.background, model.background);
-      if (model.info) Object.assign(sceneState.info, model.info);
-      if (model.floor) Object.assign(sceneState.floor, model.floor);
-      if (model.objects) sceneState.objects = model.objects;
-    });
-  }, [ideoramaid]);
+ 
 
   const soundTrack = snap.global.music.currentTrack;
   const objects = snap.objects;
