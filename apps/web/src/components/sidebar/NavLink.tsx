da5e3f97ba6e4aeb94eb19c9bbbafc062f01d4d6
Mersa Kuljici <mersa.kuljici@student.unamur.be>
2026-03-26 15:52:45 +0100

    feat(supervisor_users): added users nav link for supervisor

--- apps/web/src/components/sidebar/NavLink.tsx
+++ apps/web/src/components/sidebar/NavLink.tsx
@@ -1,4 +1,4 @@
-import { House, LayoutDashboard } from 'lucide-react';
+import { House, LayoutDashboard, LucideIcon, Users } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 import {
@@ -6,31 +6,49 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from '@/components/ui/sidebar';
+import { useUser } from '@/providers/UserProvider';
+
+interface NavLinkItem {
+  path: string;
+  label: string;
+  icon: LucideIcon;
+  role?: string;
+}
 
 export function NavLink() {
-  const links = {
+  const { user } = useUser();
+
+  const links: Record<string, NavLinkItem> = {
     mySpace: { path: 'my-space', label: 'Mon Espace', icon: House },
     home: {
       path: 'my-ideoramas',
       label: 'Mes Idéoramas',
       icon: LayoutDashboard,
     },
+    userHandling: {
+      path: 'userHandling',
+      label: 'Gestion stagiaires',
+      icon: Users,
+      role: 'SUPERVISOR',
+    },
   };
 
   return (
     <SidebarMenu className="h-fit">
-      {Object.entries(links).map(([key, link]) => (
-        <SidebarMenuItem key={key} className="my-1">
-          <SidebarMenuButton asChild className="h-auto!">
-            <Link to={link.path} className=" flex flex-col text-foreground!">
-              <div className="form-icon">
-                <link.icon size={32} className=" text-white" />
-              </div>
-              <span>{link.label}</span>
-            </Link>
-          </SidebarMenuButton>
-        </SidebarMenuItem>
-      ))}
+      {Object.entries(links)
+        .filter(([_, link]) => !link.role || user?.role === link.role)
+        .map(([key, link]) => (
+          <SidebarMenuItem key={key} className="my-1">
+            <SidebarMenuButton asChild className="h-auto!">
+              <Link to={link.path} className="flex flex-col text-foreground!">
+                <div className="form-icon">
+                  <link.icon size={32} className="text-white" />
+                </div>
+                <span>{link.label}</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        ))}
     </SidebarMenu>
   );
 }
---
+++ apps/web/src/components/users/dataTable.tsx
@@ -0,0 +1,163 @@
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  VisibilityState,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  useReactTable,
+  getPaginationRowModel,
+  getSortedRowModel,
+} from '@tanstack/react-table';
+import * as React from 'react';
+
+import { DataTablePagination } from './DataTablePagination';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Input } from '@/components/ui/input';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  return (
+    <div className="w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+      <DataTablePagination table={table} />
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter les emails..."
+          value={(table.getColumn('avatar')?.getFilterValue() as string) ?? ''}
+          onChange={event =>
+            table.getColumn('avatar')?.setFilterValue(event.target.value)
+          }
+          className="w-full sm:max-w-sm !border-mauve/80"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="outline"
+              className="ml-auto text-white! bg-mauve! hover:bg-mauve/80! !border-mauve"
+            >
+              Colonnes
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter(column => column.getCanHide())
+              .map(column => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={value => column.toggleVisibility(!!value)}
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+
+      <div className="w-full overflow-x-auto rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map(headerGroup => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map(header => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map(row => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map(cell => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  Aucun résultat.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
