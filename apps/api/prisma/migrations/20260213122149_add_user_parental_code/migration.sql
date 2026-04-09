a14dd04979e8b67aaa41e1fb94cc88c287b6c457
Blessing Tutka <benedicte.tutekamukuta@student.unamur.be>
2026-04-09 15:54:08 +0200

    refactor(migrations): remove unrelated migrations

--- apps/api/prisma/migrations/20260213122149_add_user_parental_code/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `parental_code` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "parental_code" TEXT NOT NULL;
