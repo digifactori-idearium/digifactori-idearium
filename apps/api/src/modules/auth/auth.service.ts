bce503fbd783b762bf4971be5354403f313f6add
Robin23D <robin.delvaux@student.unamur.be>
2026-04-11 17:12:55 +0200

    doc(api/auth): specifications of the service functions

--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -6,127 +6,127 @@ const userTable = prisma.user;
 const profileTable = prisma.profile;
 
 export default class AuthenticationService {
-  // Create Account
+  
+  /**
+   * Creates a new user in DB.
+   *
+   * @param inout - the user data
+   * @returns a Promise with the new user (Promise<User>)
+   */
   static async createUser(input: UserInput) {
     const { password, parental_code, ...user } = input;
 
-    try {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const hashedParentalCode = await bcrypt.hash(parental_code, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedParentalCode = await bcrypt.hash(parental_code, 10);
 
-      const newUser = await userTable.create({
+    const newUser = await userTable.create({
+      data: {
+        ...user,
+        password: hashedPassword,
+        parental_code: hashedParentalCode,
+      },
+    });
+    return newUser;
+  }
+
+  /**
+   * Creates a new profile in DB.
+   *
+   * @param ideoramaData - the profile data
+   * @returns a Promise with the new profile (Promise<Profile>)
+   */
+  static async createProfile(input: ProfileInput, userId: string) {
+    const newProfile = await profileTable.create({
+      data: {
+        ...input,
+        userId,
+      },
+    });
+
+    return newProfile;
+  }
+  /**
+   * Creates a new user and a new profile in DB.
+   *
+   * @param ideoramaData - the user and profile data
+   * @returns a Promise with the data added in DB (Promise<{ideorama: Ideorama, user: User}>)
+   */
+  static async createAccount(data: RegisterInput) {
+    const { password, parental_code, ...userData } = data.user;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    let hashedParentalCode: string | null = null;
+    if (parental_code !== undefined && parental_code !== null) {
+      hashedParentalCode = await bcrypt.hash(parental_code.toString(), 10);
+    }
+
+    const result = await prisma.$transaction(async tx => {
+      const newUser = await tx.user.create({
         data: {
-          ...user,
+          ...userData,
           password: hashedPassword,
           parental_code: hashedParentalCode,
         },
       });
 
-      return newUser;
-    } catch (error: any) {
-      throw new Error(
-        `Erreur lors de la création de l'utilisateur: ${error.message}`
-      );
-    }
-  }
-
-  static async createProfile(input: ProfileInput, userId: string) {
-    try {
-      const newProfile = await profileTable.create({
+      const newProfile = await tx.profile.create({
         data: {
-          ...input,
-          userId,
+          ...data.profile,
+          userId: newUser.id,
         },
       });
 
-      return newProfile;
-    } catch (error: any) {
-      throw new Error(
-        `Erreur lors de la création de l'utilisateur: ${error.message}`
-      );
-    }
+      return {
+        user: newUser,
+        profil: newProfile,
+      };
+    });
+
+    return result;
   }
 
-  static async createAccount(data: RegisterInput) {
-    const { password, parental_code, ...userData } = data.user;
-
-    try {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      let hashedParentalCode: string | null = null;
-      if (parental_code !== undefined && parental_code !== null) {
-        hashedParentalCode = await bcrypt.hash(parental_code.toString(), 10);
-      }
-
-      const result = await prisma.$transaction(async tx => {
-        const newUser = await tx.user.create({
-          data: {
-            ...userData,
-            password: hashedPassword,
-            parental_code: hashedParentalCode,
-          },
-        });
-
-        const newProfile = await tx.profile.create({
-          data: {
-            ...data.profile,
-            userId: newUser.id,
-          },
-        });
-
-        return {
-          user: newUser,
-          profil: newProfile,
-        };
-      });
-
-      return result;
-    } catch (error: any) {
-      throw new Error(`Erreur lors de la création du compte: ${error.message}`);
-    }
-  }
-
+  /**
+   * Gives the user with the corresponding email if the password matches, null otherwise.
+   * 
+   * @param email - the email of the user who tries to login
+   * @param password - the entered password to verify
+   * @returns a promise with the user exists and if the password is correct (Promise<User), Promise<null> otherwise
+   */
   static async loginEmail(email: string, password: string) {
-    try {
-      const user = await userTable.findUnique({
-        where: {
-          email: email,
-        },
-      });
+    const user = await userTable.findUnique({
+      where: {
+        email: email,
+      },
+    });
 
-      if (user && (await bcrypt.compare(password, user.password))) {
-        return user;
-      } else {
-        return null;
-      }
-    } catch (error: any) {
-      console.log('DB ERRORS');
-      throw new Error(
-        `Erreur lors de la vérification de l'utilisateur: ${error.message}`
-      );
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return user;
+    } else {
+      return null;
     }
   }
 
+  /**
+   * Gives the user with the corresponding pseudo if the password matches, null otherwise.
+   * 
+   * @param email - the email of the user who tries to login
+   * @param password - the entered password to verify
+   * @returns a promise with the user exists and if the password is correct (Promise<User), Promise<null> otherwise
+   */
   static async loginPseudo(pseudo: string, password: string) {
-    try {
-      const profile = await profileTable.findUnique({
-        where: {
-          pseudo,
-        },
-        include: {
-          user: true,
-        },
-      });
+    const profile = await profileTable.findUnique({
+      where: {
+        pseudo,
+      },
+      include: {
+        user: true,
+      },
+    });
 
-      if (profile && (await bcrypt.compare(password, profile.user.password))) {
-        return profile.user;
-      } else {
-        return null;
-      }
-    } catch (error: any) {
-      console.log('DB ERRORS');
-      throw new Error(
-        `Erreur lors de la vérification de l'utilisateur: ${error.message}`
-      );
+    if (profile && (await bcrypt.compare(password, profile.user.password))) {
+      return profile.user;
+    } else {
+      return null;
     }
   }
 }
