cecc1caee2eb3f1b76f26a6a8547ed295437be9f
Blessing Tutka <benedicte.tutekamukuta@student.unamur.be>
2026-04-24 15:04:17 +0200

    refactor(api/settings): update settings management

--- apps/api/src/types/index.ts
+++ apps/api/src/types/index.ts
@@ -128,7 +128,7 @@ export interface ISettingsService {
   }): Promise<Setting & { integrations: Integration[] }>;
 
   // Integrations
-  getIntegrations(): Promise<Integration[]>;
+  getIntegrations(type?: string): Promise<Integration[]>;
   getIntegrationById(integrationId: string): Promise<Integration>;
   createIntegration(data: {
     name: string;
