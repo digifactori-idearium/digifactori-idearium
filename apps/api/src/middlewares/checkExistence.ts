aadbe7d02e76e2785fb04325d85a39e8e56def02
Robin23D <robin.delvaux@student.unamur.be>
2026-04-30 19:31:04 +0200

    fix(api/middelware): try-catch dans checkExistence pour propager les erreurs

--- apps/api/src/middlewares/checkExistence.ts
+++ apps/api/src/middlewares/checkExistence.ts
@@ -1,15 +1,16 @@
 import { Ideorama, VoxelModel } from '@prisma/client';
-import type { NextFunction, Request, Response } from 'express';
+import type { NextFunction, Response } from 'express';
 
 import HttpResponse from '@/utils/http-response';
 
-export const checkIdeoramaExistence = (
-  req: Request,
+export const checkIdeoramaExistence = async (
+  ideoramaId: string,
   res: Response,
   next: NextFunction,
   getIdeoramaById: (ideoramaId: string) => Promise<Ideorama | null>
-): void => {
-  getIdeoramaById(req.body.ideoramaId).then(ideorama => {
+): Promise<void> => {
+  try {
+    const ideorama = await getIdeoramaById(ideoramaId);
     if (ideorama) {
       next();
     } else {
@@ -17,22 +18,27 @@ export const checkIdeoramaExistence = (
         res
       );
     }
-  });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const checkVoxelModelExistence = (
+export const checkVoxelModelExistence = async (
   voxelModelId: string,
   res: Response,
   next: NextFunction,
   getvoxelModelById: (voxelModelId: string) => Promise<VoxelModel | null>
-): void => {
-  getvoxelModelById(voxelModelId).then(model => {
+): Promise<void> => {
+  try {
+    const model = await getvoxelModelById(voxelModelId);
     if (model) {
       next();
     } else {
-      HttpResponse.notFound("Le modèle voxel que vous cherchez n'existe pas").send(
-        res
-      );
+      HttpResponse.notFound(
+        "Le modèle voxel que vous cherchez n'existe pas"
+      ).send(res);
     }
-  });
+  } catch (error) {
+    next(error);
+  }
 };
